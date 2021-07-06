1327a7818b14f6149c9a495a2aff0174915eec0f
sxxcxng <79037489+sxxcxng@users.noreply.github.com>
2021-07-06 15:42:44 +0900

    Add files via upload

---
+++ CharString.java
@@ -0,0 +1,19 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println("생활코딩");
+		System.out.println('생');
+		
+		System.out.println("생활코딩"+"입니다");
+		System.out.println("1"+"1");
+		
+		System.out.println("egoing said \"Welcome programming world\"");
+		System.out.println("egoing said\n \"Welcome programming world\"");
+
+
+
+	}
+
+}
